4a0ae2f8a6b58d0deab9ac834b8a80ef61027dc4
StupidHod <jememe@163.com>
2016-07-28 11:46:24 +0800

    merge etcd lease to EtcdClient

--- src/main/java/com/coreos/jetcd/EtcdClient.java
+++ src/main/java/com/coreos/jetcd/EtcdClient.java
@@ -1,6 +1,7 @@
 package com.coreos.jetcd;
 
 import com.coreos.jetcd.api.KVGrpc;
+import com.coreos.jetcd.api.LeaseGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -14,23 +15,37 @@ public class EtcdClient {
     private final ManagedChannel channel;
 
     private final EtcdKV kvClient;
+    private final EtcdLease leaseClient;
 
     public EtcdClient(ManagedChannelBuilder<?> channelBuilder, EtcdClientBuilder builder) {
-        this.endpoints = (String[])builder.endpoints().toArray();
-        this.channelBuilder = channelBuilder != null ? channelBuilder : ManagedChannelBuilder.forAddress("localhost", 2379);
+        this.endpoints = new String[builder.endpoints().size()];
+        builder.endpoints().toArray(this.endpoints);
+        this.channelBuilder = channelBuilder != null ? channelBuilder :
+                ManagedChannelBuilder
+                        .forAddress("localhost", 2379);
         this.channel = this.channelBuilder.build();
 
         KVGrpc.KVFutureStub kvStub = KVGrpc.newFutureStub(this.channel);
 
         this.kvClient = newKVClient(kvStub);
+        this.leaseClient = newLeaseClient(channel);
     }
 
-        /**
-         * create a new KV client.
-         *
-         * @return new KV client
-         */
+    /**
+     * create a new KV client.
+     *
+     * @return new KV client
+     */
     public EtcdKV newKVClient(KVGrpc.KVFutureStub stub) {
         return new EtcdKVImpl(stub);
     }
+
+    public EtcdLease newLeaseClient(ManagedChannel channel) {
+        return new EtcdLeaseImpl(channel);
+    }
+
+    public EtcdLease getLeaseClient(){
+        return leaseClient;
+    }
+
 }
